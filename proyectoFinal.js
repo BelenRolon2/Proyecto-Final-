3074a404041f5d73fa1dd0698b7a8ba3fd6d476f
BelenRolon2 <166760862+BelenRolon2@users.noreply.github.com>
2024-05-27 13:26:33 -0300

    Proyecto Final
    Entrega del proyecto final

---
+++ proyectoFinal.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function(){
+    this.document.getElementById("mostrar").addEventListener("click", function(){
+        alert("Su consulta fue enviada con exito")
+    })
+})
+
+
